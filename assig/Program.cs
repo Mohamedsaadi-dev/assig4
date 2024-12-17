171005f3c7e02ac6795d088b5580956eb389b2f4
Elmanar <Elmanar@DESKTOP-OENODU5>
2024-12-17 23:59:49 +0200

    Add project files.

---
+++ assig/Program.cs
@@ -0,0 +1,188 @@
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+using System.ComponentModel;
+using System.Diagnostics.Metrics;
+
+namespace assig
+{
+    internal class Program
+    {
+        public static object Trim { get; private set; }
+
+        static void Main(string[] args)
+        {
+            #region  Write a program that allows the user to insert an integer then print all numbers between 1 to that number.
+
+
+            // console.write("enter number :  ");
+            //int. tryparse(console.readline(), out int number);
+            // for (int i = 1; i<= number; i++) {
+            //     console.write(i);
+            //  }
+
+            #endregion
+            #region Write a program that allows the user to insert an integer then  print a multiplication table up to 12
+
+
+            //Console.Write("enter number :  ");
+            //int.TryParse(Console.ReadLine(), out int number);   
+            //for (int i = 0; i<=12; i++) {
+            //    Console.WriteLine(i*number);
+            //}
+            #endregion
+            #region
+            //Console.Write("enter number :  ");
+            //int.TryParse(Console.ReadLine(), out int number);
+            //for (int i = 0; i <= number; i++)
+            //{
+            //    if (i % 2 == 0)
+            //    {
+            //        Console.WriteLine(i);
+            //    }
+            //
+            #endregion
+            #region Write a program that takes two integers then prints the power
+            #endregion
+            #region Write a program to enter marks of five subjects and calculate total, average and percentage.
+
+            //  Console.Write("enter num: ");
+
+            //int.TryParse(Console.ReadLine(), out int num1);  
+            //int.TryParse(Console.ReadLine(), out int  num2);  
+            //int.TryParse(Console.ReadLine(), out int  num3);  
+            //int.TryParse(Console.ReadLine(), out int num4);  
+            //int.TryParse(Console.ReadLine(), out int num5);  
+            //  int sum;
+            //  sum = num1 + num2 + num3 + num4 + num5;
+            //  Console.WriteLine($"total : {sum}");
+            //  int avarage;
+            //  avarage = sum / 5;
+            //  Console.WriteLine($"avg :{avarage}");
+            //  int Percentage;
+            //  Percentage=(sum * 100) / 100;
+            //  Console.WriteLine($"Percentage : {Percentage}");
+            #endregion
+            #region  Write a program to allow the user to enter a string and print the REVERSE of it.
+            //Console.Write("enter string: ");
+            //string result = (Console.ReadLine() ?? "0");
+            //Console.WriteLine(reversed.{ result});
+            #endregion
+            #region Write a program to allow the user to enter int and print the REVERSED of it.
+            //Console.Write("enter number: ");
+            //int num=int.Parse(Console.ReadLine());
+            //Console.WriteLine(reversed.{ num});
+            #endregion
+            #region Q8
+            #endregion
+            #region Q9
+            #endregion
+            #region Create a program that asks the user to input three points (x1, y1), (x2, y2), and (x3, y3), and determines whether these points lie on a single straight line
+            //Console.WriteLine("Enter the coordinates of the three points:");
+
+            //Console.Write("Enter x1: ");
+            //double x1 = double.Parse(Console.ReadLine());
+
+            //Console.Write("Enter y1: ");
+            //double y1 = double.Parse(Console.ReadLine());
+
+            //Console.Write("Enter x2: ");
+            //double x2 = double.Parse(Console.ReadLine());
+
+            //Console.Write("Enter y2: ");
+            //double y2 = double.Parse(Console.ReadLine());
+
+            //Console.Write("Enter x3: ");
+            //double x3 = double.Parse(Console.ReadLine());
+
+            //Console.Write("Enter y3: ");
+            //double y3 = double.Parse(Console.ReadLine());
+
+            //double area = x1 * (y2 - y3) + x2 * (y3 - y1) + x3 * (y1 - y2);
+
+            //if (Math.Abs(area) < 1e-9) 
+            //{
+            //    Console.WriteLine("The points lie on a straight line.");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("The points do not lie on a straight line.");
+            //}
+            #endregion
+            #region Q11
+            #region  Write a program  Sharp to find the sum of all elements of the array.
+            //Console.Write("Enter the size of the array: ");
+            //int size = int.Parse(Console.ReadLine());
+
+
+            //int[] array = new int[size];
+
+
+            //Console.WriteLine("Enter the elements of the array:");
+            //for (int i = 0; i < size; i++)
+            //{
+            //    Console.Write($"Element [{i}]: ");
+            //    array[i] = int.Parse(Console.ReadLine());
+            //}
+
+            //int sum = 0;
+            //for (int i = 0; i < size; i++)
+            //{
+            //    sum += array[i];
+            //}
+
+            //Console.WriteLine($"The sum of all elements in the array is: {sum}");
+            #endregion
+            #region Q13
+            #endregion
+            #region Q14
+            #endregion
+            #region 15- Write a program in C# Sharp to find maximum and minimum element in an array
+
+            //Console.Write("Enter the size of the array: ");
+            //int size = int.Parse(Console.ReadLine());
+            //int[] array = new int[size];
+
+            //Console.WriteLine("Enter the elements of the array:");
+            //for (int i = 0; i < size; i++)
+            //{
+            //    Console.Write($"Element [{i}]: ");
+            //    array[i] = int.Parse(Console.ReadLine());
+            //}
+
+            //int max = array[0];
+            //int min = array[0];
+
+            //for (int i = 1; i < size; i++)
+            //{
+            //    if (array[i] > max)
+            //        max = array[i];
+            //    if (array[i] < min)
+            //        min = array[i];
+            //}
+
+            //Console.WriteLine($"The maximum element in the array is: {max}");
+            //Console.WriteLine($"The minimum element in the array is: {min}");
+            #endregion
+            #region Q16
+            #endregion
+            #region Q17
+            #endregion
+            #region Q18
+            #endregion
+            #region Q19
+            #endregion
+            #region Q20
+            #endregion
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
